21d48db3ab31c694855109eb9ee8669367e335a6
Carlos M Delgado R. <carlosmdr07@hotmail.com>
2026-04-19 23:42:53 -0600

    123

--- Proyecto/login.js
+++ Proyecto/login.js
@@ -1,26 +1,21 @@
 import { supabase } from "./supabase.js";
 
 window.login = async (event) => {
-    event.preventDefault(); // 🚨 evita que recargue
+    event.preventDefault();
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-
-    if (!email || !password) {
-        alert("Complete todos los campos");
-        return;
-    }
 
     const { data, error } = await supabase.auth.signInWithPassword({
         email,
         password
     });
 
+    // 🚨 SI HAY ERROR → NO ENTRA
     if (error) {
-        alert("Error: " + error.message);
-        return;
+        alert("Correo o contraseña incorrectos");
+        return; // 🔥 ESTO ES CLAVE
     }
-
     alert("Bienvenido 🚀");
 
     window.location.href = "inicio.html";
