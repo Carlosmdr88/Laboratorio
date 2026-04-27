21f5be93cc0d3900a620daa28ae9a0f19a7d90d6
Carlos M Delgado R. <carlosmdr07@hotmail.com>
2026-04-26 20:34:49 -0600

    logout seguridad

--- Proyecto/inicio.js
+++ Proyecto/inicio.js
@@ -4,3 +4,11 @@ window.logout = async () => {
     await supabase.auth.signOut();
     window.location.href = "./login.html";
 };
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const { data: { session } } = await supabase.auth.getSession();
+
+    if (!session) {
+        window.location.href = "./login.html";
+    }
+});
